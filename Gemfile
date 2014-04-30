6ca90aca6b5ed116f9af1e06b5a9123ff4b51be3
Valentine Zavadsky <saicheg@gmail.com>
2014-04-30 22:30:24 +0300

    fix sender

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.1.1'
 gem 'rack'
 gem 'thin'
 gem 'rest-client'
+gem 'retryable', '~> 1.3.5'
 
 group :development do
   gem 'pry'
--- lib/solver.rb
+++ lib/solver.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'rest_client'
 require 'logger'
+require 'retryable'
 
 class Solver
 
@@ -53,12 +54,15 @@ class Solver
   end
 
   def send_answer(answer, task_id)
-    uri = URI("http://pushkin-contest.ror.by/quiz")
+    retryable(tries: 3) do
+      uri = URI("http://pushkin-contest.ror.by/quiz")
 
-    data = { answer: answer, token: TOKEN, task_id: task_id}
-    options = {content_type: :json, accept: :json}
+      data = { answer: answer, token: TOKEN, task_id: task_id}
+      options = {content_type: :json, accept: :json}
 
-    RestClient.post uri.to_s, data.to_json, options
+      response = RestClient.post uri.to_s, data.to_json, options
+      raise if response.code.to_i != 200
+    end
   end
 
 end
