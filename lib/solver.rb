a8a2cf93619b1e328dcb9b078561d50f367cb348
Valentine Zavadsky <saicheg@gmail.com>
2014-04-30 21:51:29 +0300

    do only async send

--- lib/solver.rb
+++ lib/solver.rb
@@ -56,13 +56,6 @@ class Solver
         RestClient.post uri.to_s, data.to_json, options
       end
     end
-
-    uri = URI("http://pushkin-contest.ror.by/quiz")
-
-    data = { answer: answer, token: TOKEN, task_id: task_id }
-    options = {content_type: :json, accept: :json}
-
-    RestClient.post uri.to_s, data.to_json, options
   end
 
 end
