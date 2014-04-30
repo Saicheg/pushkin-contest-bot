e3c0c56b0dcdc438b517bec3ac9bb57952d5d8b5
Valentine Zavadsky <saicheg@gmail.com>
2014-04-30 22:54:42 +0300

    use net http

--- lib/solver.rb
+++ lib/solver.rb
@@ -63,7 +63,7 @@ class Solver
   def send_answer(answer, task_id)
     retryable(tries: 3) do
       data = { answer: answer, token: TOKEN, task_id: task_id}
-      @http.post('/quiz', data)
+      @http.post('/quiz', data.to_json, {'Content-Type' =>'application/json'})
     end
   end
 
